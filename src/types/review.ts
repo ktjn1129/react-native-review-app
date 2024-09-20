4ae7e4251589e26312ae36c7d7920a7c8ff70dd4
Jinichiro Koto <jinichirokoto@JinichironoMacBook-Pro.local>
2024-09-20 18:34:04 +0900

    add create review screen

---
+++ src/types/review.ts
@@ -0,0 +1,17 @@
+type UserRef = {
+  id: string;
+  name: string;
+};
+
+type ShopRef = {
+  id: string;
+  name: string;
+};
+
+export type Review = {
+  id?: string;
+  text: string;
+  score: number;
+  user: UserRef;
+  shop: ShopRef;
+};
