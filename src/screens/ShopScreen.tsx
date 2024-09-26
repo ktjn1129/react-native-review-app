a7fca7f210c827504527d1609d1a7c29c8eb4195
Jinichiro Koto <jinichirokoto@JinichironoMacBook-Pro.local>
2024-09-26 22:51:31 +0900

    add display review function

--- src/screens/ShopScreen.tsx
+++ src/screens/ShopScreen.tsx
@@ -1,10 +1,13 @@
 import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
 import { ShopDetail } from "../components/ShopDetail";
 import { FloatingActionButton } from "../components/FloatingActionButton";
 import { RootStackParamList } from "../types/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ReviewItem } from "../components/ReviewItem";
+import { Review } from "../types/review";
+import { review } from "../../mock/data";
 
 type Props = {
   navigation: StackNavigationProp<RootStackParamList, "Shop">;
@@ -13,14 +16,22 @@ type Props = {
 
 export const ShopScreen: React.FC = ({ navigation, route }: Props) => {
   const { shop } = route.params;
+  const [reviews, setReviews] = useState<Review[]>([]);
 
   useEffect(() => {
     navigation.setOptions({ title: shop.name });
+
+    setReviews(review);
   });
 
   return (
     <SafeAreaView style={styles.container}>
-      <ShopDetail shop={shop} />
+      <FlatList
+        ListHeaderComponent={<ShopDetail shop={shop} />}
+        data={reviews.filter((review) => review.shop.id === shop.id)}
+        renderItem={({ item }) => <ReviewItem review={item} />}
+        keyExtractor={(item) => item.id}
+      />
       <FloatingActionButton
         iconName="plus"
         onPress={() => navigation.navigate("CreateReview", { shop })}
